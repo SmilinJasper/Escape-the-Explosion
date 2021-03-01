abd92ddaad10903eb079210df4c3bb937d039d3b
Smilin-Jasper <d.sjd.sjj@gmail.com>
2021-03-01 23:21:04 +0530

    font-loading-optimized-woff-woff2

--- game.js
+++ game.js
@@ -55,7 +55,22 @@ function drawStartScreen() {
     document.addEventListener("keydown", displayHighScoreBoard);
 }
 
-const gameOverFont = new FontFace("Game Over", "url(font/game_over_regular.woff2)");
+var gameOverFont = new FontFace("Game Over", "url(font/game_over_regular.woff)");
+
+var supportsWoff2 = (function() {
+    if (!("FontFace" in window)) {
+        return false;
+    }
+
+    var f = new FontFace('t', 'url( "data:font/woff2;base64,d09GMgABAAAAAADwAAoAAAAAAiQAAACoAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABmAALAogOAE2AiQDBgsGAAQgBSAHIBuDAciO1EZ3I/mL5/+5/rfPnTt9/9Qa8H4cUUZxaRbh36LiKJoVh61XGzw6ufkpoeZBW4KphwFYIJGHB4LAY4hby++gW+6N1EN94I49v86yCpUdYgqeZrOWN34CMQg2tAmthdli0eePIwAKNIIRS4AGZFzdX9lbBUAQlm//f262/61o8PlYO/D1/X4FrWFFgdCQD9DpGJSxmFyjOAGUU4P0qigcNb82GAAA" ) format( "woff2" )', {});
+    f.load()['catch'](function() {});
+
+    return f.status == 'loading' || f.status == 'loaded';
+})();
+
+if (supportsWoff2) {
+    var gameOverFont = new FontFace("Game Over", "url(font/game_over_regular.woff2)");
+}
 
 background.onload = () => gameOverFont.load().then(font => {
     document.fonts.add(font);
